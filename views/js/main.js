073ca09e7d2e1a8cc609ec53bfe07592108c91fe
unknown <Evan.Ponter@AS-MD-A11489.astrum.astrumsolar.com>
2015-06-10 00:42:19 -0400

    calcualated docheight once

--- views/js/main.js
+++ views/js/main.js
@@ -518,7 +518,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
@@ -541,7 +542,9 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var docHeight = window.innerHeight;
+  
+  for (var i = 0; i < docHeight; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
